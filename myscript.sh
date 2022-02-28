3dd5b1cdaf3481e2073e510b6014405451744a6b
MuthomiGuantai <muthomiguantai@gmail.com>
2022-02-28 21:34:37 +0300

    varibles

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/bash
 
 #ECHO COMMAND
-echo Hello World!
+#echo Hello World!
+
+# VARIABLES
+# Uppercase by convention
+# Letters, numbers, undersores
+NAME="Bruce"
+echo "My name is $NAME"
